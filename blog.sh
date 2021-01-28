91776c9e7bc03c5453acd2ff600e5eecbed4cbaf
nitong <nitong@digisky.com>
2021-01-28 18:44:11 +0800

    auto push

--- blog.sh
+++ blog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /etc/profile
+
 if test "$1" = "run"; then
   port=8080
   if [ $2 ]; then
@@ -10,7 +12,8 @@ elif test "$1" = "build"; then
   bundle exec jekyll build --destination=dist
 elif test "$1" = "deploy"; then
   bundle exec jekyll build --destination=dist
-  ~/bin/blog-deploy-linux -src=./dist
+  # ~/bin/blog-deploy-linux -src=./dist
+  git pull && git add -A && git commit -m 'auto push' && git push
 else
   echo "error param"
 fi
--- pages/about.md
+++ pages/about.md
@@ -2,3 +2,14 @@
 layout: mypost
 title: 关于我
 ---
+> 欢迎访问 Travis 个人博客
+
+大家好，我是一名`运维工程师`，在这里我可能会分享一些有关运维方面的知识，也可能会写一些闲碎的生活片段。
+
+# 技能点
+linux k8s ansible git jenkins python shell
+
+# 联系我
+QQ    : 1315984007
+Email : liuyuexizhi@qq.com
+GitHub: https://github.com/liuyuexizhi
