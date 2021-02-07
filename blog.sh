d20f050ac8cdd838388c4d9052e166a2c59e7625
nitong <nitong@digisky.com>
2021-02-07 19:58:30 +0800

    auto push

--- blog.sh
+++ blog.sh
@@ -22,8 +22,8 @@ elif test "$1" = "build"; then
   bundle exec jekyll build --destination=dist
 elif test "$1" = "deploy"; then
   echo "#####################$(date +"%Y-%m-%d-%H:%M:%S")###########################"
-  bundle exec jekyll build --destination=dist
   bash src/format.sh
+  # bundle exec jekyll build --destination=dist
   git pull && git add -A && git commit -m 'auto push' && git push
   echo -e "####################################################################\n\n"
 else
